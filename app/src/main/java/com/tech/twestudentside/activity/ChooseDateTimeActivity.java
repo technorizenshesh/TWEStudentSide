a6fa49e9d690a5d1f196e98c9f200e6197fb85ad
Harshit <harshit.ixora89@gmail.com>
2021-04-24 20:24:51 +0530

    All File 24 Aprl harshit

--- app/src/main/java/com/tech/twestudentside/activity/ChooseDateTimeActivity.java
+++ app/src/main/java/com/tech/twestudentside/activity/ChooseDateTimeActivity.java
@@ -1,25 +1,20 @@
 package com.tech.twestudentside.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.tech.twestudentside.R;
 
 public class ChooseDateTimeActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_date_time);
     }
 
     public void ContinueDateAndTimeId(View view) {
-
-        startActivity(new Intent(ChooseDateTimeActivity.this, LessonSummaryActivity.class));
-
+        startActivity(new Intent(this, LessonSummaryActivity.class));
     }
 
     public void backDate(View view) {
